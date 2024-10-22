779211d191c1a5b32b64020bf9f24686f39a3394
Umberto Tramontano <u.tramontano92@libero.it>
2024-10-22 12:59:47 +0200

    update resultPageScript.js

--- JS/resultPageScript.js
+++ JS/resultPageScript.js
@@ -1,11 +1,11 @@
 //variabili con il numero di risposte corrette ed errate
-let correctAnswers = 6
-let wrongAnswers = 4
+let correctAnswers = 8
+let wrongAnswers = 3
 let questionNumbers = correctAnswers + wrongAnswers
 let correctPercentage = (correctAnswers / questionNumbers) * 100
 let wrongPercentage = (wrongAnswers / questionNumbers) * 100
-let roundedCorrectPercentage = correctPercentage.toFixed(0)
-let roundedWrongPercentage = wrongPercentage.toFixed(0)
+let roundedCorrectPercentage = correctPercentage.toFixed(2)
+let roundedWrongPercentage = wrongPercentage.toFixed(2)
 
 //riferimento per il button del doc HTML
 const rateUs = document.getElementById('rate-us')
@@ -33,7 +33,12 @@ numWrongAns.innerHTML = `
 `
 
 const text1 = correctPercentage >= 60 ? 'Congratulations!' : 'Sorry!'
-const text2 = correctPercentage >= 60 ? 'Exam passed' : 'Try later!'
+const text2 =
+  correctPercentage >= 60 ? 'You passed the exam' : 'Try later again!'
+const text3 = correctPercentage >= 60 ? `We'll send you the certificate` : ``
+const text4 = correctPercentage >= 60 ? `in few minutes.` : ``
+const text5 = correctPercentage >= 60 ? `Check your email(including` : ``
+const text6 = correctPercentage >= 60 ? `promotions/spam folder)` : ``
 
 //riferimento al tag canvas tramite il suo ID
 let ctx = document.getElementById('myChart').getContext('2d')
@@ -51,15 +56,36 @@ const centerTextPlugin = {
     ctx.save()
     ctx.textAlign = 'center'
     ctx.textBaseline = 'top'
-    ctx.font = 'bold 16px Arial' // Modifica il font a piacere
+    ctx.font = 'bold 20px Outfit' // Modifica il font a piacere
     ctx.fillStyle = '#fff' // Colore del testo
 
     // Inserisci il testo che vuoi al centro
-    ctx.fillText(text1, centerX, centerY - 100)
+    ctx.fillText(text1, centerX, centerY - 80)
 
     //seconda riga di testo
     ctx.fillStyle = '#00FFFF'
-    ctx.fillText(text2, centerX, centerY - 80)
+    ctx.fillText(text2, centerX, centerY - 60)
+
+    //terza riga di testo
+    ctx.fillStyle = '#FFFFFF'
+    ctx.font = 'normal 14px Inter'
+    ctx.fillText(text3, centerX, centerY)
+
+    //quarta riga di testo
+    ctx.fillStyle = '#FFFFFF'
+    ctx.font = 'normal 14px Inter'
+    ctx.fillText(text4, centerX, centerY + 20)
+
+    //quinta riga di testo
+    ctx.fillStyle = '#FFFFFF'
+    ctx.font = 'normal 14px Inter'
+    ctx.fillText(text5, centerX, centerY + 40)
+
+    //sesta riga di testo
+    ctx.fillStyle = '#FFFFFF'
+    ctx.font = 'normal 14px Inter'
+    ctx.fillText(text6, centerX, centerY + 60)
+
     ctx.restore()
   },
 }
